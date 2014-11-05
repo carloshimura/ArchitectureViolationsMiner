2356970b8661a1b0ba6a71cd97edbc322905f627
carloshimura <carloshimura@gmail.com>
2014-11-05 19:41:35 -0200

    Modificações e arquivos de resultados adicionados

--- src/org/violations/Main.java
+++ src/org/violations/Main.java
@@ -1,13 +1,15 @@
 package org.violations;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
 		ViolationsManager l_manager = new ViolationsManager();
-		l_manager
-				.add_file(
-						"01",
-						"/ArchitectureViolationsMiner/trunk/data/violations/ant_violations/ant_week_014_rev_500752_date_2007-01-28.txt");
+//		l_manager
+//				.add_file(
+//						"01",
+//						"/ArchitectureViolationsMiner/trunk/data/violations/ant_violations/ant_week_014_rev_500752_date_2007-01-28.txt");
 //		l_manager
 //				.add_file(
 //						"02",
@@ -160,7 +162,29 @@ public class Main {
 //				.add_file(
 //						"39",
 //						"/ArchitectureViolationsMiner/trunk/data/violations/ant_violations/ant_week_052_rev_626825_date_2007-10-21.txt");
-		 l_manager.write_graph();
+		
+		
+//		File folder = new File("/ArchitectureViolationsMiner/trunk/data/violations/ant_violations/");
+		File folder = new File("/ArchitectureViolationsMiner/trunk/data/violations/argouml_violations/");
+//		File folder = new File("/ArchitectureViolationsMiner/trunk/data/violations/lucene_violations/");
+//		File folder = new File("/ArchitectureViolationsMiner/trunk/data/violations/sweethome3d_violations/");
+		 File[] listOfFiles = folder.listFiles();
+
+		 int i = 1;
+		 for (File file : listOfFiles) {
+		     if (file.isFile()) 
+		     {
+		    	 String l_version = i + "";
+		    	 while(l_version.length() < 2)
+		    		 l_version = "0" + l_version;
+		    	 l_manager.add_file(l_version, file.getAbsolutePath());
+		    	 ++i;
+		     }
+		 }
+
+
+		
+		l_manager.write_graph();
 		//l_manager.write_degree();
 	}
 
